a3364a963b08dce33ad99326eb84931d077f3d5c
jrog20 <jaimerogstad@gmail.com>
2021-10-09 12:17:26 -0400

    Add helper methods

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,40 @@ class Tag < ApplicationRecord
 
   validate :name, :validate_name
 
+  scope :grouped_by_tag_id, -> { joins(:dish_tags).group(:tag_id) }
+  scope :ordered_by_popularity_desc, -> { grouped_by_tag_id.order("COUNT(dish_tags.dish_id) DESC") }
+  scope :ordered_by_popularity_asc, -> { grouped_by_tag_id.order("COUNT(dish_tags.dish_id) ASC") }
+
+  # tag with the most associated dishes
+  scope :most_common, -> { ordered_by_popularity_desc.take }
+
+  # tag with the fewest associated dishes
+  scope :least_common, -> { ordered_by_popularity_asc.take }
+
+  # all tags that have been used
+  scope :used, -> { joins(:dish_tags).distinct } 
+
+  # all tags that haven’t been used
+  scope :unused, -> { where.not(id: used) }
+
+  # all tags that have been used fewer than 5 times
+  scope :uncommon, -> { grouped_by_tag_id.having("COUNT(dish_tags.dish_id) < ?", 5) }
+
+  # top 5 tags by use
+  scope :popular, -> { ordered_by_popularity_desc.take(5) }
+  
+  # restaurants that have this tag on at least one dish
+  scope :restaurants_for_tag, -> (tag) { Restaurant.joins(:dishes => :tags).where("dish_tags.tag_id = ?", tag.id) }
+
+  def restaurants
+    Tag.restaurants_for_tag(self).distinct
+  end
+
+  # restaurant that uses this tag the most
+  def top_restaurant
+    Tag.restaurants_for_tag(self).group(:restaurant_id).order("COUNT(restaurant_id) DESC").take
+  end
+
   private
   
   # Add validations to Tag name: Must be at least three characters,
